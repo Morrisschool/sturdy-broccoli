a88af53d517898fd08c7c7ae309f2b25b91eb2aa
Morrisschool <93544921+Morrisschool@users.noreply.github.com>
2022-08-23 12:38:30 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,34 @@
+docker_install() {
+    printf "\x1B[01;93m========== Updating system ==========\n\x1B[0m"
+    sudo apt update
+
+    printf "\x1B[01;93m========== Install make and docker ==========\n\x1B[0m"
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo apt-get update
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt install -y make docker-ce docker-ce-cli docker-compose containerd.io
+
+    printf "\x1B[01;93m========== Ensure keys exist ==========\n\x1B[0m"
+    # Create .ssh/ if it doesn't exist
+    [ -d ~/.ssh/ ] || mkdir ~/.ssh
+    # Generate passwordless keys if they don't exist
+    [ -f ~/.ssh/id_rsa ] || ssh-keygen -N "" -f ~/.ssh/id_rsa
+    # Create an authorized_keys file if it doesn't exist
+    [ -f ~/.ssh/authorized_keys ] || touch ~/.ssh/authorized_keys
+    # Add our key to it if it is not present
+    KEY=$(cat ~/.ssh/id_rsa.pub)
+    grep -Fxq "$KEY" ~/.ssh/authorized_keys || cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+
+    printf "\x1B[01;92m================== Done.  ==================\n\x1B[0m\n\n"
+}
+
+#Check if distro is tested, warn if not.
+if is_tested; then
+echo
+else
+    printf "\n\033[0;31mUntested operating system detected! You may press Ctrl+C now to abort this script.\nInstallation will proceed in 10 seconds.\n\n"
+    sleep 10
+fi
+
+# Actually do the install. Put in function and run at end to prevent parcial download and execution.
+docker_install
