6bf00553a37f70a3327070260393a97f6158eb20
Morrisschool <93544921+Morrisschool@users.noreply.github.com>
2022-08-28 06:07:48 -0700

    Create install2.sh

---
+++ install2.sh
@@ -0,0 +1,14 @@
+install() {
+    printf "\x1B[01;93m========== Updating system ==========\n\x1B[0m"
+    sudo apt update
+    sudo apt upgrade
+
+    printf "\x1B[01;93m========== Install HaProxy 2.6 and dependencies ==========\n\x1B[0m"
+    sudo apt install --no-install-recommends software-properties-common
+    sudo add-apt-repository ppa:vbernat/haproxy-2.6 -y
+    sudo apt update
+    sudo apt install haproxy=2.6.\* -y
+
+}
+# Actually do the install. Put in function and run at end to prevent parcial download and execution.
+install
