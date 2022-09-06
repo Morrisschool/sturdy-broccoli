2aa25bdcf0791ca2527a0180704dd699c4f54810
Morrisschool <93544921+Morrisschool@users.noreply.github.com>
2022-09-06 15:14:27 -0700

    Update installproxy.sh

--- installproxy.sh
+++ installproxy.sh
@@ -2,6 +2,7 @@ install() {
     printf "\x1B[01;93m========== Updating system ==========\n\x1B[0m"
     sudo apt update
     sudo apt upgrade -y
+    sudo apt-get install -y open-vm-tools
 
     printf "\x1B[01;93m========== Install HaProxy 2.6 and dependencies ==========\n\x1B[0m"
     sudo apt install --no-install-recommends software-properties-common
