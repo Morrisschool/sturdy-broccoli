f8e6f97a858c81522e2a54ef49d730df14689a03
Morrisschool <93544921+Morrisschool@users.noreply.github.com>
2022-09-06 15:10:22 -0700

    Create installvpn.sh

---
+++ installvpn.sh
@@ -0,0 +1,14 @@
+install() {
+    printf "\x1B[01;93m========== Updating system ==========\n\x1B[0m"
+    sudo apt update
+    sudo apt upgrade -y
+
+    printf "\x1B[01;93m========== Install OpenVPN and dependencies ==========\n\x1B[0m"
+    sudo apt -y install ca-certificates wget net-tools gnupg
+    wget -qO - https://as-repository.openvpn.net/as-repo-public.gpg | sudo apt-key add -
+    echo "deb http://as-repository.openvpn.net/as/debian jammy main" > /etc/apt/sources.list.d/openvpn-as-repo.list
+    sudo apt update && sudo apt -y install openvpn-as
+
+}
+# Actually do the install. Put in function and run at end to prevent parcial download and execution.
+install
