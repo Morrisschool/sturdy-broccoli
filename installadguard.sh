16dbced490056ccabefd5e382ec8b12f8aba4556
Morrisschool <93544921+Morrisschool@users.noreply.github.com>
2022-09-25 04:58:55 -0700

    Update installadguard.sh

--- installadguard.sh
+++ installadguard.sh
@@ -2,14 +2,14 @@ adguard_install() {
     printf "\x1B[01;93m========== Updating system ==========\n\x1B[0m"
     sudo apt update
     sudo apt upgrade -y
-    sudo apt-get install -y open-vm-tools
+    sudo apt-get install -y open-vm-tools curl
 
     printf "\x1B[01;93m========== Install AdGuard ==========\n\x1B[0m"
     curl -s -S -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -v
 
     printf "\x1B[01;93m========== Free up port 53 on 0.0.0.0 ==========\n\x1B[0m"
     sudo mkdir -p /etc/systemd/resolved.conf.d
-    echo -e "[Resolve]\nDNS=127.0.0.1\nDNSStubListener=no" > /etc/systemd/resolved.conf.d/adguardhome.conf
+    sudo sh -c "echo -e "[Resolve]\nDNS=127.0.0.1\nDNSStubListener=no" > /etc/systemd/resolved.conf.d/adguardhome.conf"
     sudo mv /etc/resolv.conf /etc/resolv.conf.backup
     sudo ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
 
