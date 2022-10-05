ab6fc2ed49588cd398410876d7b820cdc40694ea
Morrisschool <93544921+Morrisschool@users.noreply.github.com>
2022-10-05 11:39:30 -0700

    Create sshtunnel_activation.sh

---
+++ sshtunnel_activation.sh
@@ -0,0 +1,32 @@
+#! /usr/bin/env bash
+
+sshtunnel_activation() {
+	printf "\x1B[01;93m========== Tunnel name ==========\n\x1B[0m"
+	TUNNEL=$(ls /etc/systemd/system/ | grep tunnel)
+	read -p "Is this the correct tunnel?" YN
+	
+	case $YN in
+		[yY] ) printf "\x1B[01;93m========== Reload daemon, enable service on startup, start service ==========\n\x1B[0m"
+			sudo systemctl daemon-reload
+			sudo systemctl enable $TUNNEL
+			sudo systemctl start $TUNNEL
+    
+			printf "\x1B[01;92m================== Done.  ==================\n\x1B[0m\n\n"
+			sudo systemctl status $TUNNEL
+			break;;
+		[nN] ) read -p "Please enter the name of the tunnel you want to start: " TUNNELNAME
+			printf "\x1B[01;93m========== Reload daemon, enable service on startup, start service ==========\n\x1B[0m"
+			sudo systemctl daemon-reload
+			sudo systemctl enable $TUNNELNAME
+			sudo systemctl start $TUNNELNAME
+    
+			printf "\x1B[01;92m================== Done.  ==================\n\x1B[0m\n\n"
+			sudo systemctl status $TUNNELNAME
+			break;;
+		* ) echo invalid response;;
+	esac
+	
+	done
+}
+
+sshtunnel_activation
