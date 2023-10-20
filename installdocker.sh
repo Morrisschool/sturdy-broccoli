8b46be0139de71246cae91eb8386da590fa44a1d
Morrisschool <93544921+Morrisschool@users.noreply.github.com>
2023-10-21 01:01:50 +0200

    Update installdocker.sh

--- installdocker.sh
+++ installdocker.sh
@@ -7,11 +7,13 @@ docker_install() {
     sudo apt install -y open-vm-tools apt-transport-https ca-certificates curl gnupg lsb-release
 
     printf "\x1B[01;93m========== Install make and docker ==========\n\x1B[0m"
+    sudo apt-get update
+    sudo install -m 0755 -d /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+    echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt update
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt update
-    sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
     printf "\x1B[01;93m========== Ensure keys exist ==========\n\x1B[0m"
     # Create .ssh/ if it doesn't exist
