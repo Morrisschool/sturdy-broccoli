febe33005bfd6baa7081a3df017e4f5fc9cd183b
Morrisschool <93544921+Morrisschool@users.noreply.github.com>
2024-06-30 02:48:59 -0700

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+
+install() {
+    printf "\x1B[01;93m========== Updating system ==========\n\x1B[0m"
+    sudo apt update
+    sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
+
+    printf "\x1B[01;92m================== Rebooting...  ==================\n\x1B[0m\n\n"
+    sudo reboot now
+
+    printf "\x1B[01;92m================== Done.  ==================\n\x1B[0m\n\n"
+}
+
+# Actually do the install. Put in function and run at end to prevent parcial download and execution.
+install
